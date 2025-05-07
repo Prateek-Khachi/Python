2393c235b0c869556b54e95a6ce8dc81c8fc51bf
Prateek-Khachi <145461958+Prateek-Khachi@users.noreply.github.com>
2025-05-07 15:05:34 +0530

    Update HW.py

--- HW.py
+++ HW.py
@@ -1,3 +1,15 @@
-is_weekend = False
-if not is_weekend:
-    print("It's a weekday")
+# is_weekend = False
+# if not is_weekend:
+#     print("It's a weekday")
+
+# try:
+#     num = int(input("Enter a number: "))
+# except ValueError:
+#     print("Please enter a number next time.")
+#     quit()
+# finally:
+#     print("Thank You.") 
+ 
+fruits = ["apple", "mango", "bananas"]
+del fruits[0]
+print(fruits)
