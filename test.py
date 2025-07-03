65cb7c018e855af31417d5d4db1e6339a40752a7
Prateek-Khachi <145461958+Prateek-Khachi@users.noreply.github.com>
2025-07-03 22:27:20 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+from collections import Counter
+import heapq
+
+# lst = [1,1,1,2,2,3]
+# print(f"This is the {Counter(lst)} map.")
+
+# mpp = {1: 3, 2: 2, 3: 1}
+# print(f"This is the {mpp} map.")
+
+
+# lidt = [7, 2, 23, 4, 57]
+
+# lidt = [-x for x in lidt]
+# print(lidt)
+# heapq.heapify(lidt)
+# print(lidt)
+# heapq.heappush(lidt, -1)
+# print(lidt)
+# lidt = [-x for x in lidt]
+# print(lidt)
+
+# matrix = [[1, 2], [3, 4], [5, 6]]
+
+# for i, j in matrix:
+#     print(i, j)
+
+ans = "hello"
+rev = reversed(ans)
+print(rev)  # Output: <reversed object at 0x...>
+
+
